ef7dff340aadb851aeca82a1eb2a1c42a98bfbc7
Michael Chess <michael.chess@columbia.edu>
2018-04-29 17:16:43 -0400

    first paramertisfoisdfs version

--- mod_image_section.py
+++ mod_image_section.py
@@ -6,12 +6,16 @@ print("###Starting###")
 
 directory = sys.argv[1]
 out_dir = sys.argv[2]
+attack_type = sys.argv[3]
+param = int(sys.argv[4])
+category = int(sys.argv[5])
+
 
 def main():
 	for filename in os.listdir(directory):
 		if filename.endswith('.jpg'):
 			print()
-			num = 154
+			num = category
 
 			obj = Image.open(directory+'/'+filename[:-4]+'_object.png')
 			img = Image.open(directory+'/'+filename)
@@ -21,33 +25,42 @@ def main():
 
 			if num in obj_arr:
 				print filename
-				obj_arr[obj_arr != num] = 0
-				obj_arr[obj_arr == num] = 1
+				mask = np.zeros(obj_arr.shape)
+				# obj_arr[obj_arr != num] = 0
+				mask[obj_arr == num] = 1
 				
 				#print obj_arr.shape
-				obj_arr = obj_arr.repeat(2, axis=0).repeat(2, axis=1)
-				obj_arr = np.repeat(obj_arr[:, :, np.newaxis], 3, axis=2)
+				mask = mask.repeat(2, axis=0).repeat(2, axis=1)
+				mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
 				#print obj_arr.shape
 
-				
+				img_arr = np.array(np.array(img), dtype= np.uint32)
+				print img_arr.dtype
+				if attack_type == "brightening":
+					# print np.unique(obj_arr.flatten())
 
-				# print np.unique(obj_arr.flatten())
+					# mod_img = Image.fromarray(obj_arr)
 
-				# mod_img = Image.fromarray(obj_arr)
+					# mod_img.show()
 
-				# mod_img.show()
+					# masked_img = np.ma.masked_array(img_arr, mask=obj_arr)
+					
 
-				img_arr = np.array(img)
-				# masked_img = np.ma.masked_array(img_arr, mask=obj_arr)
-				
+					#img_arr[obj_arr!=1] = 0
 
-				#img_arr[obj_arr!=1] = 0
-
-				img_arr = np.add(img_arr, obj_arr * 50)
+					img_arr = np.add(img_arr, mask * param)
+				elif attack_type == "gaussian":
+					noise_old = np.random.normal(0.0, param, img_arr.shape)
+					noise_old[mask == 0] = 0
+					noise = np.array(noise_old, dtype = np.float32)
+					print noise.dtype
+					img_arr = np.add(img_arr, noise)
 				img_arr[img_arr>255] = 255
+				img_arr[img_arr<0] = 0
+				img_arr = np.array(img_arr, dtype = np.uint8)
 
 
-				# Insert Attack Here
+				# Insert new Attack Here
 
 				#print img_arr.shape
 				mod_img = Image.fromarray(img_arr)
